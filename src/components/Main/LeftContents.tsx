9da684985219fd57bcff0fbe86384124eabb08b9
tboy4all <taiyesuleiman09@gmail.com>
2022-09-03 17:07:24 +0100

    implemented LightBox and fixed scrollbar

--- src/components/Main/LeftContents.tsx
+++ src/components/Main/LeftContents.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef } from 'react'
 import { useAppDispatch, useAppSelector } from '../../app/hooks'
 import { togglePictureIndex } from './contentSlice'
 import LightBox from './LightBox'
